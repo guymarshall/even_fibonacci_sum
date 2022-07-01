5ed6fdff57bdd1d067b077d179cb26017b2d246f
Guy Marshall <guy@cslgroupservices.co.uk>
2022-07-01 12:27:52 +0100

    Fix issue with 1 less

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 
 # By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
-def fibonacci(number - 1):
+def fibonacci(number):
     first = 0
     second = 1
     count = 0
